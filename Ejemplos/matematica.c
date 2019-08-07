f4d29c1acb07240692a2f9e488e61ce0e5e0b2eb
Gonzalo Cortes <gdcortes@uc.cl>
2019-08-07 00:27:22 -0400

    matematicas completas (falta solucionar float)

--- Ejemplos/matematica.c
+++ Ejemplos/matematica.c
@@ -2,8 +2,15 @@
 
 int main(void)
 {
-  int resultado;
-  resultado=5+2
-  printf("Resultado de la suma: %i\n",resultado);
+  int resultado1;
+  float resultado2;
+  resultado1=5+2;
+  printf("Resultado de la suma: %i\n",resultado1);
+  resultado1=5-2;
+  printf("Resultado de la resta: %i\n",resultado1);
+  resultado1=5*2;
+  printf("Resultado de la multiplicacion: %i\n",resultado1);
+  resultado2=5/2;
+  printf("Resultado de la division: %f\n",resultado2);
   return 0;
 }
